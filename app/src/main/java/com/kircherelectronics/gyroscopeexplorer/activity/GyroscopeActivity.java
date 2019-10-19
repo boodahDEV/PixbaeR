fbb3cdf86f24011de410eb59e5d89552a4358ff8
Moisés Chiari <mdchiari@MacBook-Pro-de-Moises.local>
2019-10-19 18:16:49 -0500

    giroscopio

--- app/src/main/java/com/kircherelectronics/gyroscopeexplorer/activity/GyroscopeActivity.java
+++ app/src/main/java/com/kircherelectronics/gyroscopeexplorer/activity/GyroscopeActivity.java
@@ -41,28 +41,7 @@ import org.apache.commons.math3.complex.Quaternion;
 import java.util.Arrays;
 import java.util.Locale;
 
-/*
- * Copyright 2013-2017, Kaleb Kircher - Kircher Engineering, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * The main activity displays the orientation estimated by the sensor(s) and
- * provides an interface for the user to modify settings, reset or view help.
- *
- * @author Kaleb
- */
 public class GyroscopeActivity extends AppCompatActivity implements SensorEventListener {
     private static final String tag = GyroscopeActivity.class.getSimpleName();
 
